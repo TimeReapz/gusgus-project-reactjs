3319cf522b2c0a5b6fc9c9db5f74c06203e86f68
TimeReapz <souldarkfear@gmail.com>
2021-08-12 22:19:45 +0700

    now

--- src/components/pages/manage-users/Create.js
+++ src/components/pages/manage-users/Create.js
@@ -1,104 +1,115 @@
 import axios from "axios";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import firebase from "../utils/firebase";
 
+export default function Create() {
+  const [name, setName] = useState('');
 
-export default class Create extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      id: props.match.params.id ?? 0,
-    };
-  }
+  // useEffect(() => {
+  //   if (id) {
+  //     axios
+  //       .get(process.env.REACT_APP_HOST_API + `/api/user/${id}`)
+  //       .then((response) => {
+  //         this.name.value = response.data.name;
+  //       });
+  //   }
+  // }, []);
 
-  componentDidMount() {
-    const id = this.state.id;
-    if (id) {
-      axios.get(process.env.REACT_APP_HOST_API + `/api/user/${id}`).then((response) => {
-        this.name.value = response.data.name;
-      });
-    }
-  }
-
-  saveUser = (e) => {
-    e.preventDefault();
-    var id = this.state.id;
-    if (id !== 0) {
-      // update
-      axios
-        .patch(process.env.REACT_APP_HOST_API + `/api/user/${id}`, {
-          name: this.name.value,
-        })
-        .then((response) => {
-          this.props.history.push('/manageuser');
-        });
-    } else {
-      // insert
-      axios
-        .post(process.env.REACT_APP_HOST_API + `/api/user`, {
-          name: this.name.value,
-        })
-        .then((response) => {
-          this.props.history.push('/manageuser');
-        });
-    }
+  const handleOnChange = (e) => {
+    setName(e.target.value);
   };
 
-  render() {
-    return (
-      <div>
-        <section className="content-header">
-          <div className="container">
-            <div className="row mb-2">
-              <div className="col-sm-6">
-                <h1>รายชื่อลูกค้า</h1>
-              </div>
+  const saveUser = () => {
+    const modelRef = firebase.database().ref('User');
+    const model = {
+      NAME: name,
+      CREATE_DATE: new Date(),
+      ISACTIVE: 1,
+      complete: false,
+    };
+
+    modelRef.push(model);
+  };
+
+  // const saveUser = (e) => {
+  //   e.preventDefault();
+  //   var id = this.state.id;
+  //   if (id !== 0) {
+  //     // update
+  //     axios
+  //       .patch(process.env.REACT_APP_HOST_API + `/api/user/${id}`, {
+  //         name: this.name.value,
+  //       })
+  //       .then((response) => {
+  //         this.props.history.push("/manageuser");
+  //       });
+  //   } else {
+  //     // insert
+  //     axios
+  //       .post(process.env.REACT_APP_HOST_API + `/api/user`, {
+  //         name: this.name.value,
+  //       })
+  //       .then((response) => {
+  //         this.props.history.push("/manageuser");
+  //       });
+  //   }
+  // };
+
+  return (
+    <div>
+      <section className="content-header">
+        <div className="container">
+          <div className="row mb-2">
+            <div className="col-sm-6">
+              <h1>รายชื่อลูกค้า</h1>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
 
-        <section className="content">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <div className="card card-outline card-primary">
-                  <div className="card-header">
-                    <div className="card-title">
-                      <h3 className="card-title">เพิ่มรายชื่อลูกค้า</h3>
+      <section className="content">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="card card-outline card-primary">
+                <div className="card-header">
+                  <div className="card-title">
+                    <h3 className="card-title">เพิ่มรายชื่อลูกค้า</h3>
+                  </div>
+                </div>
+                <div className="card-body table-responsive p-0">
+                  <form onSubmit={saveUser}>
+                    <div className="card-body">
+                      <div className="form-group">
+                        <label htmlFor="name">ชื่อ</label>
+                        <input
+                          type="name"
+                          className="form-control"
+                          id="name"
+                          placeholder="ชื่อลูกค้า"
+                          value={name} 
+                          onChange={handleOnChange}
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="card-body table-responsive p-0">
-                    <form onSubmit={this.saveUser}>
-                      <div className="card-body">
-                        <div className="form-group">
-                          <label htmlFor="name">ชื่อ</label>
-                          <input
-                            type="name"
-                            className="form-control"
-                            id="name"
-                            placeholder="ชื่อลูกค้า"
-                            ref={(input) => (this.name = input)}
-                          />
-                        </div>
+                    <div className="card-footer">
+                      <Link to="/manageuser" className="btn btn-danger mr-2">
+                        Back
+                      </Link>
+                      <div className="float-right">
+                        <button type="submit" className="btn btn-primary">
+                          Submit
+                        </button>
                       </div>
-                      <div className="card-footer">
-                        <Link to="/manageuser" className="btn btn-danger mr-2">
-                          Back
-                        </Link>
-                        <div className="float-right">
-                          <button type="submit" className="btn btn-primary">
-                            Submit
-                          </button>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    );
-  }
+        </div>
+      </section>
+    </div>
+  );
 }
