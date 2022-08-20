2fe93d612c293dd880c8121dcbd28dc2564a7274
TimeReapz <53538865+TimeReapz@users.noreply.github.com>
2022-08-21 00:28:07 +0700

    build! fix footer menu style

--- src/components/_layout/MenuTop.js
+++ src/components/_layout/MenuTop.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { AuthContext } from "../pages/authentication/Auth";
 import { firebase } from "../../utils/firebase";
 
